b8fa4c606e61d3fb1da7fef094da3d36ac5559f4
OronAvisrur <oronavisrur@gmail.com>
2025-04-23 19:40:42 +0300

    Restructured project: moved backend files into /backend

--- backend/api.js
+++ backend/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./database/db'); // Import the MongoDB connection function
 
 const app = express();
@@ -6,6 +7,10 @@ const port = 8000;
 
 // Connect to MongoDB
 connectDB();
+
+// Set Pug as the view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware to parse JSON requests
 app.use(express.json());
