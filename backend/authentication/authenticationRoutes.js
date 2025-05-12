a1d1cf91004c34487d7f354dc692e911a3d9a13e
OronAvisrur <oronavisrur@gmail.com>
2025-05-12 18:13:04 +0300

    Added fullReport route and fixed authenticationRoutes file

--- backend/authentication/authenticationRoutes.js
+++ backend/authentication/authenticationRoutes.js
@@ -34,4 +34,28 @@ router.post('/login', login);
  */
 router.post('/register', register);
 
+/**
+ * Route to handle user deletion.
+ * @name DELETE /delete
+ * @function
+ * @memberof module:authenticationRoutes
+ * @inner
+ * @param {express.Request} req - Express request object.
+ * @param {express.Response} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ */
+router.delete('/delete', deleteUser);
+
+/**
+ * Route to handle updating user credentials.
+ * @name PUT /update
+ * @function
+ * @memberof module:authenticationRoutes
+ * @inner
+ * @param {express.Request} req - Express request object.
+ * @param {express.Response} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ */
+router.put('/update', update);
+
 module.exports = router;
